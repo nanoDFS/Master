08bf979cfc395c7dadcc447f0562a7e9bfa85137
nagarajRPoojari <np137270@gmail.com>
2025-03-07 22:15:50 +0530

    avoid inactive servers

--- controller/metadata/chunkserver/chunker.go
+++ controller/metadata/chunkserver/chunker.go
@@ -27,7 +27,7 @@ func (t *Chunker) Generate(fileId string, size int64) []*ChunkServer {
 
 	var servers []*ChunkServer
 
-	allChunkServers := GetChunkServerMetadata().GetAllChunkServers()
+	allChunkServers := GetChunkServerMetadata().GetAllActiveChunkServers()
 	for i := range count {
 		index, err := t.startegy.GetIndex(lb.Opts{Key: fileId + fmt.Sprint(i), Length: len(allChunkServers)})
 		if err != nil {
